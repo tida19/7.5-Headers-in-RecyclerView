9fefff8879218d69ff9f79aac7d8554cfbaf82e7
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-09-06 02:47:24 +0700

    7.5-6-Task: Extend the header to span across the screen

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -1,19 +1,6 @@
-/*
- * Copyright 2019, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.trackmysleepquality.sleeptracker
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -102,7 +89,6 @@ class SleepTrackerFragment : Fragment() {
                 sleepTrackerViewModel.doneNavigating()
             }
         })
-
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
                 adapter.addHeaderAndSubmitList(it)
@@ -116,8 +102,19 @@ class SleepTrackerFragment : Fragment() {
                 sleepTrackerViewModel.onSleepDetailNavigated()
             }
         })
+
         val manager = GridLayoutManager(activity, 3)
+
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) =  when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
+
         binding.sleepList.layoutManager = manager
+
+
         return binding.root
     }
 }
